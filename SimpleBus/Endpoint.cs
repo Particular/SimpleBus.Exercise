c3002471fffffab1d58c0f8287fb33c96d2334a0
Andreas Öhlund <andreas.ohlund@particular.net>
2025-04-28 14:23:51 +0200

    Use primary ctor

--- SimpleBus/Endpoint.cs
+++ SimpleBus/Endpoint.cs
@@ -1,18 +1,7 @@
 namespace SimpleBus;
 
-public sealed class Endpoint
+public sealed class Endpoint(string name, ITransportMessages transport, ISerializeMessages serializer)
 {
-    private readonly string name;
-    private readonly ISerializeMessages serializer;
-    private readonly ITransportMessages transport;
-
-    public Endpoint(string name, ITransportMessages transport, ISerializeMessages serializer)
-    {
-        this.name = name;
-        this.transport = transport;
-        this.serializer = serializer;
-    }
-
     public void Register<TMessage>(Func<TMessage, Task> receiver)
     {
         var type = typeof(TMessage).FullName ?? throw new InvalidOperationException("Message type full name is null.");
